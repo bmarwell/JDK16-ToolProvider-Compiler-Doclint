b35232a6d0e576f3dd5b96160a1013e9aecc5f99
Benjamin Marwell <bmarwell@gmail.com>
2020-12-15 10:05:01 +0100

    run on multiple JDKs.

--- run.sh
+++ run.sh
@@ -5,14 +5,8 @@ isodate() {
   date --iso-8601=s
 }
 
-echo "$(isodate) [INFO ] checking for java 16"
-
-set +e
-if ! java -version 2>&1 | head -1 | grep -sq "\"16"; then
-  echo "$(isodate) [ERROR] java-version is not 16!"
-  exit 1
-fi
-set -e
+echo "$(isodate) [INFO ] java -version"
+java -version 2>&1
 
 echo "$(isodate) [INFO ] Cleaning up old class files"
 rm -rf src/main/java/*.class
